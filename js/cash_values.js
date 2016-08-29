32c7b453444f39b942e1a2e3351610ea82a8ce6d
Anna Grochola <grochola.anna@gmail.com>
2016-08-29 23:29:50 +0200

    dashboard working and formated api

--- js/cash_values.js
+++ js/cash_values.js
@@ -7,43 +7,34 @@
 
 $(document).ready(function() {
 
-    var button = document.getElementById("button1");
-    var input = document.getElementById("pass");
+    function ajaxPost( data, endpoint, method, success ) {      //opakowanie ajaxa w funkcję, dzięki czemu możemy do środka zamiast sztywnych danych przekazać argumenty
 
-    $(button).on('click', function (event){
-        event.preventDefault();
-        var password = $(input).val();      //pobieramy wartość inputa
-        sendAjax(password);             //przekazujemy argument do funkcji sendAjax
-    });
-
-    function sendAjax (pass) {
-        if (pass.length > 0) {
-
-            $.ajax({
-                type: "post",
-                data: {
-                    login: "efi",
-                    password: pass    //takie hasło ma być wysyłane do api, jakie użytkownik wpisze, ale w API jest hasło "camp" i tylko takie będzie dobre
-                },
-                url: "https://efigence-camp.herokuapp.com/api/login",
-                error: function (response) {
-                    console.log(response.responseText);
-                },
-                success: function (response) {
-                    console.log(response);
-                }
+        $.ajax({                        //funkcja jQuery, ma zawierać to, co zawiera nasze API
+                method: method,         //GET lub POST
+                url: "https://efigence-camp.herokuapp.com/api/"+endpoint, //endpoint - końcówka adresu; naszym adresem dla API jest ...herokuapp... + /data/summary, czyli endpoint
+                data: data          //czyli dodatkowe dane, np. w przypadku loginpage to login i hasło
+            })
+            .done(function (msg) {      //funkcja, która wykonuje się jako sukces
+                success(msg);
             });
-        }
-
-        else { //ma się pojawić pod inputem element html, czerwone tło, biały napis "proszę wpisać hasło"
-            $(button).click(function(){
-                $(".no_pass").toggleClass("pass_in");
-            });
-
-
-        };
     };
 
+
+    ajaxPost(null, "data/summary", "GET", function (msg) {      //wywołujemy jako funckję anonimową funkcję wyżej, przy czym podajemy jej, co chcemy w console.log, a to wiemy z API
+        var apiBalance = msg.content[0].balance;
+        var apiFunds = msg.content[0].funds;
+        var apiPayments = msg.content[0].payments;
+
+        var numberWithSpaces = function (x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        };
+
+        document.getElementById("balance").innerHTML = (numberWithSpaces(apiBalance)+",00").bold() + " PLN";
+        document.getElementById("funds").innerHTML = (numberWithSpaces(apiFunds)+",00").bold() + " PLN";
+        document.getElementById("payments").innerHTML = (numberWithSpaces(apiPayments)+",00").bold() + " PLN";
+
+        }
+    );
 
 
 })
@@ -57,24 +48,3 @@ $(document).ready(function() {
 // endpoint - do nich wysyłamy zapytanie, na które otrzymujemy odpowiedź  zawartą na githubie w obiekcie
 
 //co się zmienia: url,
-
-
-
-function ajaxPost( data, endpoint, method, success ) {      //opakowanie ajaxa w funkcję, dzięki czemu możemy do środka zamiast sztywnych danych przekazać argumenty
-
-    $.ajax({                        //funkcja jQuery, ma zawierać to, co zawiera nasze API
-            method: method,         //GET lub POST
-            url: "https://efigence-camp.herokuapp.com/api/"+endpoint, //endpoint - końcówka adresu; naszym adresem dla API jest ...herokuapp... + /data/summary, czyli endpoint
-            data: data          //czyli dodatkowe dane, np. w przypadku loginpage to login i hasło
-        })
-        .done(function (msg) {      //funkcja, która wykonuje się jako sukces
-            success(msg);
-        });
-};
-
-ajaxPost(null, "data/summary", "GET", function (msg) {      //wywołujemy jako funckję anonimową funkcję wyżej, przy czym podajemy jej, co chcemy w console.log, a to wiemy z API
-        console.log("Callback pozdrawia");
-        console.log(msg);
-
-    }
-);
